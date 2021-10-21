d49ab76399c2663b7367d2ae23de702e25cbc381
prmpsmart <prmpsmart@gmail.com>
2021-10-21 12:48:26 +0100

    included .gitignore

---
+++ py/starter.py
@@ -0,0 +1,8 @@
+from os import mkdir, walk, makedirs, path, getcwd
+
+
+f = r'C:\Users\Administrator\Coding_Projects\Python\Dev_Workspace\Qt_Material_Widgets\cpp\qt-material-widgets-master\components'
+try:mkdir('components')
+except: ...
+
+
