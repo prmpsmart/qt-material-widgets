4eaa6a94d6dc24e99a3a2d02576a3cea2a26c48e
prmpsmart <prmpsmart@gmail.com>
2021-10-21 13:43:57 +0100

    created all the folders, I'll be continuing later in the day

--- py/starter.py
+++ py/starter.py
@@ -1,8 +1,18 @@
 from os import mkdir, walk, makedirs, path, getcwd
 
 
-f = r'C:\Users\Administrator\Coding_Projects\Python\Dev_Workspace\Qt_Material_Widgets\cpp\qt-material-widgets-master\components'
-try:mkdir('components')
-except: ...
+f = r"C:\Users\Administrator\Coding_Projects\Python\Dev_Workspace\Qt_Material_Widgets\cpp\qt-material-widgets-master\components"
+try:
+    mkdir("examples")
+    mkdir("components")
+except:
+    ...
 
-
+for a, b, c in walk(f):
+    for ba in b:
+        g = path.join(a, ba).replace(f, "components")
+        # g = path.join("components", g)
+        try:
+            makedirs(g)
+        except:
+            ...
